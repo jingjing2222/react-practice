c50ee4cc2db27739c59fe841bbbcf1dc64ce54ab
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-01-10 09:45:48 +0900

    수정 맛만 봄

--- src/components/AaronLecture2.jsx
+++ src/components/AaronLecture2.jsx
@@ -3,14 +3,14 @@ import { useEffect, useRef } from 'react'
 function AaronLecture2() {
     const reference = useRef(null)
 
-    console.log('렌더링 전 : ')
-    console.log(reference.current)
-    console.log(reference.current?.innerText)
+    // console.log('렌더링 전 : ')
+    // console.log(reference.current)
+    // console.log(reference.current?.innerText)
 
     useEffect(() => {
-        console.log('렌더링 후 : ')
-        console.log(reference.current)
-        console.log(reference.current.innerText)
+        // console.log('렌더링 후 : ')
+        // console.log(reference.current)
+        // console.log(reference.current.innerText)
     }, [])
 
     return (
