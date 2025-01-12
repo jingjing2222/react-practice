297ff275bf9f29c2d318fe38064bc868e6e28e3e
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-01-12 19:40:48 +0900

    handleSubmit button식 적용 완료

--- src/components/test/GetTest3.jsx
+++ src/components/test/GetTest3.jsx
@@ -1,13 +1,68 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 export default function GetTest3() {
+    const [show, setShow] = useState('Form')
+    const onChangeHandler = (e) => {
+        const newShow = e.target.value
+        setShow(newShow)
+        console.log(newShow)
+    }
+    return (
+        <>
+            <div style={{ fontSize: 25 }}>GetTest2</div>
+            <hr />
+            <select style={{ display: 'block' }} onChange={onChangeHandler}>
+                <option value='Form'>Form</option>
+                <option value='Element'>Element</option>
+            </select>
+            <Choice show={show} />
+        </>
+    )
+}
+
+function Choice({ show }) {
     const { register, handleSubmit } = useForm()
     /*register = form안에 데이터 처리, <input type='password' {...register('password')}> */
-    /* handleSubmit form안에 submit에 연동됨 반드시 form에 쓸것
+    /* handleSubmit form안에 submit에 연동됨, button onClick에도 가능한
     <form onsubmit={handlesubmit(()=>console.log(콜백함수입니다.))}> 내부에는 callback 함수가 들어가야함*/
     const onSubmit = (data) => console.log(data)
+    {
+        return show === 'Form' ? (
+            <Type1 register={register} handleSubmit={handleSubmit} onSubmit={onSubmit} />
+        ) : (
+            <Type2 register={register} handleSubmit={handleSubmit} onSubmit={onSubmit} />
+        )
+    }
+}
+
+function Type1({ register, handleSubmit, onSubmit }) {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor='id'>ID : </label>
+            <input
+                id='id'
+                type='id'
+                placeholder='ID'
+                {...register('id')} //{id:{value}}
+            ></input>
+            <div />
+            <label htmlFor='passsword'>password : </label>
+            <input
+                id='password'
+                type='password'
+                placeholder='password'
+                {...register('password')} //{password:{value},}
+            ></input>
+            <div></div>
+            <button type='submit'>제출</button>
+        </form>
+    )
+}
+
+function Type2({ register, handleSubmit, onSubmit }) {
+    return (
+        <>
             <label htmlFor='id'>ID : </label>
             <input id='id' type='id' placeholder='ID' {...register('id')}></input>
             <div />
@@ -19,7 +74,9 @@ export default function GetTest3() {
                 {...register('password')}
             ></input>
             <div></div>
-            <button type='submit'>제출</button>
-        </form>
+            <button type='submit' onClick={handleSubmit(onSubmit)}>
+                제출
+            </button>
+        </>
     )
 }
